a253305ef1b9e170635e96472d32aab7a314d336
AndrewPochapsky <andrew.pochapsky@gmail.com>
2022-03-15 21:34:53 -0400

    Update to use name of generated project

--- examples/schema.rs
+++ examples/schema.rs
@@ -1,9 +1,9 @@
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+use {{crate_name}}::{export_schema, remove_schemas, schema_for};
 
-use contract_starter::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use {{crate_name}}::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
